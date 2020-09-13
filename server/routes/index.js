56b1c3d51d9184d4e007a917266591bac561970f
xian <uxian554@gmail.com>
2020-09-13 09:51:28 +0200

    add profile route

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,6 +14,7 @@ const productsRoute = require("./productsRoute");
 const purchasesRoute = require("./purchasesRoute");
 const salesRoute = require("./salesRoute");
 const suppliersRoute = require("./suppliersRoute");
+const profileRoute = require("./profileRoute");
 
 const momoRoute = require("./momoRoute");
 
@@ -46,7 +47,7 @@ app.use("/api/v1/", productsRoute);
 app.use("/api/v1/", purchasesRoute);
 app.use("/api/v1/", salesRoute);
 app.use("/api/v1/", suppliersRoute);
-
+app.use("/api/v1/", profileRoute);
 //Momo Payment
 app.use("/api/v1/", momoRoute);
 
