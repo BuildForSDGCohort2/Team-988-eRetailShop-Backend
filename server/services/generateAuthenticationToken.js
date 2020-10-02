aa2bf2bbadddcf71da4fa5ca4e524bfe4b871787
xian <uxian554@gmail.com>
2020-10-02 14:01:41 +0200

    set token expiration to 1 hour

--- server/services/generateAuthenticationToken.js
+++ server/services/generateAuthenticationToken.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const jwt = require("njwt");
 
+//3600000 milliseconds correponds to 3600 sec to 1 hr
 const generateAuthToken = (payload) => {
   const token = jwt.create(payload, process.env.jwtPrivateKey);
-  token.setExpiration(new Date().getTime() + 60 * 1000);
+  token.setExpiration(new Date().getTime() + 3600000);
   return token.compact();
 };
 
